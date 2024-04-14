3b323fe37e3bb3d702e0c1a4d2a043490c66fa49
Shanawaj hossain <princesaju645@gmail.com>
2024-04-14 21:26:39 +0600

    fetch all image

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,8 +1,13 @@
+import PhotoList from "@/components/PhotoList";
 import { getDictionary } from "./dictionaries/disctionaries";
 
 export default async function Home({params:{lang}}) {
   const dictionary=await getDictionary(lang)
+
+  const response=await fetch(`${process.env.BASE_API_URL}/photos`);
+
+  const photos=await response.json();
   return (
-    <div>{dictionary.followers}</div>
+    <PhotoList photos={photos}/>
   );
 }
